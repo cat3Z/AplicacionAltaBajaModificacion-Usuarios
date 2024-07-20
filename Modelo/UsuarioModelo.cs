daea9757507b26561c8d877796dc0beb832af6cc
catscu <88331081+catscu@users.noreply.github.com>
2024-07-20 15:05:50 -0300

    Se agrega modelo y controlador de usuario y función de alta

---
+++ Modelo/UsuarioModelo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class UsuarioModelo : Modelo
+    {
+        public int Id;
+        public string Nombre;
+        public string Apellido;
+
+        public void GuardarUsuario()
+        {
+            string sql = $"INSERT INTO usuarios (nombre,apellido) VALUES('{this.Nombre}',{this.Apellido})";
+            this.Comando.CommandText = sql;
+            this.Comando.ExecuteNonQuery();
+        }
+
+    }
+}
